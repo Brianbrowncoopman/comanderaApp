c8f7e53ecb0c90bd543d7d372df0743bb7191616
brianbrown <brianbrowncoopman@gmail.com>
2025-12-15 23:41:40 -0300

    muchos cambios

--- app/src/main/java/cl/brbc/example/comanderaapp/MainActivity.kt
+++ app/src/main/java/cl/brbc/example/comanderaapp/MainActivity.kt
@@ -1,23 +1,128 @@
 package cl.brbc.example.comanderaapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import cl.brbc.example.comanderaapp.modelo.CuentaMesa
+import cl.brbc.example.comanderaapp.modelo.ItemMenu.Companion.cazuela
+import cl.brbc.example.comanderaapp.modelo.ItemMenu.Companion.pastel_choclo
+import cl.brbc.example.comanderaapp.modelo.ItemMenu.Companion.precio_cazuela
+import cl.brbc.example.comanderaapp.modelo.ItemMenu.Companion.precio_pastel
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     /*variables*/
+    private var switchCalcularPropina: Switch = null
+    private var etCantidadPastel: EditText? = null
+    private var etCantidadCazuela: EditText? = null
+    private var tvTotalUnidad1: TextView? = null
+    private var tvTotalUnidad2: TextView? = null
+    private var tvTotalComida: TextView? = null
+    private var tvTotalPropina: TextView? = null
+    private var tvTotalNumerico: TextView? = null
+
+    private val cuentaMesa = CuentaMesa()
+
+    private val currencyFormat: NumberFormat = NumberFormat.getCurrencyInstance(Locale("es", "CL"))
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        /*enableEdgeToEdge()*/
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        inicializarVistas()
+
+        configurarListeners()
+
+        actualizarTotalesUnidad(0, 0)
+        actualizarTotalesCuenta()
+
+    }
+
+    private fun inicializarVistas() {
+        switchCalcularPropina = findViewById(R.id.switch1)
+        etCantidadPastel = findViewById(R.id.etncantidad1)
+        etCantidadCazuela = findViewById(R.id.etncantidad2)
+
+        tvTotalUnidad1 = findViewById(R.id.tvTotalUnidad1)
+        tvTotalUnidad2 = findViewById(R.id.tvTotalUnidad2)
+        tvTotalComida = findViewById(R.id.tvTotalComida)
+        tvTotalPropina = findViewById(R.id.tvTotalPropina)
+        tvTotalNumerico = findViewById(R.id.tvTotalNumerico)
+
+    }
+
+    private fun configurarListeners() {
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int after: Int){}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int){}
+            override fun afterTextChanged(s: Editable){
+                calcularCuenta()
+            }
+        }
+
+        etCantidadPastel?.addTextChangedListener(textWatcher)
+        etCantidadCazuela?.addTextChangedListener(textWatcher)
+
+        switchCalcularPropina?.setOnCheckedChangeListener { _, isChecked ->
+            cuentaMesa.aceptaPropina = isChecked
+            calcularCuenta()
         }
     }
+
+    private fun obtenerCantidad(editText: EditText?): Int {
+
+        val texto = editText?.text.toString()
+        return texto.toIntOrNull() ?: 0
+    }
+
+    private fun calcularCuenta() {
+
+        val cantidadPastel = obtenerCantidad(etCantidadPastel)
+        val cantidadCazuela = obtenerCantidad(etCantidadCazuela)
+
+        val subTotalPastel = cantidadPastel * precio_pastel
+        val subTotalCazuela = cantidadCazuela * precio_cazuela
+
+        actualizarTotalesUnidad(subtotalPastel, subTotalCazuela)
+
+        cuentaMesa.getItems().clear()
+
+        if (cantidadPastel > 0) {
+            cuentaMesa.agregarItem(pastel_choclo , cantidadPastel)
+        }
+        if (cantidadCazuela > 0) {
+            cuentaMesa.agregarItem(cazuela, cantidadCazuela)
+        }
+
+        actualizarTotalesCuenta()
+
+    }
+
+    private fun actualizarTotalesUnidad(totalPastel: Int, totalCazuela: Int) {
+        tvTotalUnidad1?.text = currencyFormat.format(totalPastel)
+        tvTotalUnidad2?.text = currencyFormat.format(totalCazuela)
+    }
+
+    private fun actualizarTotalesCuenta() {
+
+        val totalComidaSinPropina = cuentaMesa.calcularTotalSinPropina()
+        val totalPropina = cuentaMesa.calcularPropina()
+        val totalFinal = cuentaMesa.calcularTotalConPropina()
+
+        tvTotalComida?.text = currencyFormat.format(totalComidaSinPropina)
+        tvTotalPropina?.text = currencyFormat.format(totalPropina)
+        tvTotalNumerico?.text = currencyFormat.format(totalFinal)
+
+    }
+
+
+
 }
--- app/src/main/java/cl/brbc/example/comanderaapp/modelo/comanda.kt
+++ app/src/main/java/cl/brbc/example/comanderaapp/modelo/comanda.kt
@@ -1,2 +1,80 @@
 package cl.brbc.example.comanderaapp.modelo
 
+class ItemMenu(val nombre: String, val precio: Int) {
+    companion object {
+        const val precio_pastel = 12000
+        const val precio_cazuela = 10000
+
+        val pastel_choclo = ItemMenu("Pastel de choclo" , precio_pastel)
+        val cazuela = ItemMenu("Cazuela", precio_cazuela)
+    }
+}
+
+/*
+ * 2. Clase ItemMesa (Representa el pedido de la mesa)  arrastra con itemMenu(el nombre y el precio)
+ * y guarda en cantidad la cantidad de ese plato
+ */
+class ItemMesa(val itemMenu: ItemMenu, val cantidad: Int) {
+
+    /*
+     * Funcion para calcular el subtotal (multiplica itemMenu.precio(solo el precio de itemMenu) contra la cantidad)
+     * y retorna el suctotal (como entero)
+     */
+    fun calcularSubtotal(): Int {
+        return itemMenu.precio * cantidad
+    }
+}
+
+/*
+ * -----------------------------------------------------------
+ * 3. Clase CuentaMesa (Gestiona el total de la cuenta) guarda en variable privada el numero de la mesa
+ * _item es una lista (array) que almacena los pedidos
+ * -----------------------------------------------------------
+ */
+class CuentaMesa(private val mesa: Int = 1) {
+
+    // +_items : MutableList<ItemMesa>
+    private val _items: MutableList<ItemMesa> = mutableListOf()
+
+    // +aceptaPropina : Boolean = true
+    var aceptaPropina: Boolean = true
+
+
+    /*Funcion para agregarItem (arrastra el nombre y el precio con itemMenu y la cantidad de itemMenu con cantidad)*/
+    fun agregarItem(itemMenu: ItemMenu, cantidad: Int) {
+        if (cantidad > 0) {   /*evalua si la cantidad es mayor a 0 */
+            val itemMesa = ItemMesa(itemMenu, cantidad) /*en val itemMesa se almacena el nombre,
+                                                         *  el valor y la cantidad(2 parametros(1 de ellos tiene 2 parametros))*/
+            _items.add(itemMesa) /**si cantidad es mayor a 0  alm,acena itemMesa en el array _items*/
+        }
+    }
+
+
+    fun agregarItem(itemMesa: ItemMesa) {
+        _items.add(itemMesa)
+    }
+
+
+    fun calcularTotalSinPropina(): Int {
+        return _items.sumOf { it.calcularSubtotal() }
+    }
+
+
+    fun calcularPropina(): Int {
+        if (aceptaPropina) {
+            val totalSinPropina = calcularTotalSinPropina()
+            // 10% de propina
+            return totalSinPropina / 10
+        }
+        return 0
+    }
+
+
+    fun calcularTotalConPropina(): Int {
+        return calcularTotalSinPropina() + calcularPropina()
+    }
+
+
+    fun getItems(): List<ItemMesa> = _items.toList()
+}
+
